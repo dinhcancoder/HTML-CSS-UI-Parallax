d1420a2a2f8a13ece0b3e8584d18c8022ace5cba
CaoDinhCan <89732863+CaoDinhCan@users.noreply.github.com>
2024-03-06 01:00:29 +0700

    Parallax

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+    const x = (window.innerWidth - e.pageX * speed) / 100;
+    const y = (window.innerHeight - e.pageY * speed) / 100;
+    layer.style.transform = `translate(${x}px, ${y}px)`;
+  });
+}
